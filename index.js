3d69b96f86dcb72817ce352c015868792894bf49
Ratchanon Semsayan <Ratchanon.se@sbac.ac.th>
2026-02-03 10:51:44 +0700

    add method get, post on path /department

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+let department = [
+  { id: 1, name: "IT" },
+  { id: 2, name: "DBT" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/department", (req, res) => {
+  res.send(department);
+});
+
+app.post("/department", (req, res) => {
+  let newDepartment = {
+    id: department.length + 1,
+    name: req.body.name,
+  };
+  department.push(newDepartment);
+  res.send(newDepartment);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
