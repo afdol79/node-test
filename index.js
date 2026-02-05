7b1ddf6e0506d4f6d12853a637b2605dc921f114
afdol79 <ratchanon.se@sbac.ac.th>
2026-02-05 12:30:56 +0700

    Example complete

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/department", (req, res) => {
-  res.send(department);
+  res.json(department);
 });
 
 app.post("/department", (req, res) => {
@@ -23,8 +23,30 @@ app.post("/department", (req, res) => {
     name: req.body.name,
   };
   department.push(newDepartment);
-  res.send(newDepartment);
+  res.json(newDepartment);
 });
+
+app.put('/department/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let dept = department.findIndex(d => d.id === id);
+  if (id !== -1) {
+    department[dept].name = req.body.name;
+    res.json(department[dept]);
+  } else {
+    res.status(404).send('Department not found');
+  }
+})
+
+app.delete('department/:id',(req,res)=>{
+  let id = parseInt(req.params.id);
+  let deptIndex = department.findIndex(d => d.id === id);
+  if(deptIndex !== -1){
+    let deletedDept = department.splice(deptIndex, 1);
+    res.json(deletedDept[0]);
+  } else {
+    res.status(404).send('Department not found');
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
